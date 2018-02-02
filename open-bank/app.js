ead41e73cb79a2a8c04b176c6f702e5d3e5cf73f
Rick Clark <rkingsleyclark@gmail.com>
2018-02-02 14:19:53 +0000

    Fix env issue

--- open-bank/app.js
+++ open-bank/app.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const request = require('request-promise-native');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-
 const getAuthString = ({ username, password }) =>
   `DirectLogin username=${username}, password=${password}, consumer_key=${process
     .env.OPEN_BANK_API_KEY}`;
 
 const getAuthTokenString = token => `DirectLogin token="${token}"`;
 
+app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(
@@ -20,25 +20,9 @@ app.use(
   }),
 );
 
-// used for webops healthchecks & deployment.
-app.get('/private/ping', (req, res) => {
-  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.status(200).send('pong');
-});
-
-app.post('/login', (req, res) => {
-  login(req)
-    .then(token => getAccounts(token))
-    .then(({ accounts, token }) => getBankAccounts(accounts, token))
-    .then(data => res.status(200).json(data))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
-
 function login(req) {
   console.log('login');
+  console.log(req.body);
   const options = {
     uri: 'https://apisandbox.openbankproject.com/my/logins/direct',
     headers: {
@@ -82,18 +66,35 @@ function getBankAccounts(accounts, token) {
   return Promise.all(accounts.map(account => getBankAccount(account, token)));
 }
 
-function getTransactions({ accounts, token }) {
-  console.log('getTransactions', token);
-  const options = {
-    uri: 'https://apisandbox.openbankproject.com/obp/v3.0.0/my/accounts',
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      Authorization: getAuthTokenString(token),
-    },
-    json: true, // Automatically parses the JSON string in the response
-  };
+// function getTransactions({ accounts, token }) {
+//   console.log('getTransactions', token);
+//   const options = {
+//     uri: 'https://apisandbox.openbankproject.com/obp/v3.0.0/my/accounts',
+//     headers: {
+//       'Content-Type': 'application/json; charset=utf-8',
+//       Authorization: getAuthTokenString(token),
+//     },
+//     json: true, // Automatically parses the JSON string in the response
+//   };
 
-  return request.get(options);
-}
+//   return request.get(options);
+// }
+
+// used for webops healthchecks & deployment.
+app.get('/private/ping', (req, res) => {
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.status(200).send('pong');
+});
+
+app.post('/login', (req, res) => {
+  login(req)
+    .then(token => getAccounts(token))
+    .then(({ accounts, token }) => getBankAccounts(accounts, token))
+    .then(data => res.status(200).json(data))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
 module.exports = app;
