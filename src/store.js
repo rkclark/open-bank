c214ce9cc3e575fff250f11a98d826ef97489750
Rick Clark <rkingsleyclark@gmail.com>
2018-02-02 13:33:43 +0000

    Create mortgage form reducer and fields

--- src/store.js
+++ src/store.js
@@ -2,8 +2,9 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import login from './reducers/login';
+import mortgageForm from './reducers/mortgageForm';
 
 export default createStore(
-  combineReducers({ login }),
+  combineReducers({ login, mortgageForm }),
   composeWithDevTools(applyMiddleware(thunkMiddleware)),
 );
